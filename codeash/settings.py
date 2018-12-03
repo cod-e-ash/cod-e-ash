6ef504e3abc71389fd26124fb29326acc8cac496
Ashish Bagaddeo <ashish@bagaddeo.com>
2018-12-03 05:58:46 +0000

    Settings Changed

--- codeash/settings.py
+++ codeash/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(01*7nq=f^l9o6o(eyqrhdyfr57p!vo2!_(d3qoqc#m7-5m-xj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.zs2pg3jtzs.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.zs2pg3jtzs.us-west-2.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
