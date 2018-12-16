212ef8cf03fb2ce5fc078db26941a5f353ef33d9
Ashish Bagaddeo <ashish@bagaddeo.com>
2018-12-16 04:53:07 +0000

    Deploy

--- codeash/settings.py
+++ codeash/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(01*7nq=f^l9o6o(eyqrhdyfr57p!vo2!_(d3qoqc#m7-5m-xj'
-
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
